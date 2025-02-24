4010793389645ee242cf3651e37c2120b55991b6
Anchal2003 <sahuanchal22@gmail.com>
2025-02-24 23:17:06 +0530

    [Anchal Sahu] Add UC1- Create a Employee Payroll Spring Project to cater to REST Request from Employee Payroll UI.

--- WelcomeToSpringFramework.java
+++
@@ -1,7 +0,0 @@
-//Create a WelcomeToSpringFramework to print the welcome statement
-public class WelcomeToSpringFramework {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to the Spring Framework\"");
-    }
-
-}
