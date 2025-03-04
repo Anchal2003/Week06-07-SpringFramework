ead0da3d900bb07e9c07811854671c27264a1126
Anchal2003 <sahuanchal22@gmail.com>
2025-03-04 12:28:17 +0530

    [Anchal Sahu] Add Section2 UC1- Introducing DTO and Model to Employee Payroll App.

--- src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplicationTests.java
+++ src/test/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.bridgelabz.employeepayrollapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Use @SpringBootTest Annotation
 @SpringBootTest
+//Create a class EmployeePayrollAppApplicationTests to perform test
 class EmployeePayrollAppApplicationTests {
 
 	@Test
