1e0e3ba349b5f8459e84a44a92c2bd0e808c2881
Anchal2003 <sahuanchal22@gmail.com>
2025-03-04 11:12:24 +0530

    [Anchal Sahu] Add Controller file to handle the api request and respones.

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -1,17 +1,19 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
+import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.entity.Employee;
 import com.bridgelabz.employeepayrollapp.service.EmployeePayrollServices;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 //Use Annotations
 @RestController
 @RequestMapping("/employeepayrollservice")
-
+@Slf4j
 //create a class EmployeePayrollController to handle the request and response
 public class EmployeePayrollController {
 
@@ -30,20 +32,22 @@ public class EmployeePayrollController {
     //Use @GetMapping Annotation to get the details
     @GetMapping("/get/{id}")
     //Create a method to get the employee details by the id
-    public Optional<Employee> getEmployeeDetails(@PathVariable Long id) {
+    public ResponseEntity<EmployeeDTO> getEmployeeDetails(@PathVariable Long id) {
         //Call the method and return the value
         return useServices.findDetailsById(id);
     }
 
     @GetMapping("/all")
-    public List<Employee> getAllRecord(){
+    public List<EmployeeDTO> getAllRecord(){
         return useServices.findAllRecords();
     }
 
     //Use @PostMapping Annotation
     @PostMapping("/create")
     //Create a method createEmployeeRecord to create an employee record in database
-    public Employee createEmployeeRecord(@RequestBody Employee employee) {
+    public ResponseEntity<Employee> createEmployeeRecord(@RequestBody Employee employee) {
+        //Use logger to give the information
+        log.info("Creating new  Employee Record");
         //Call the method and return the value
         return useServices.createRecord(employee);
     }
@@ -51,7 +55,9 @@ public class EmployeePayrollController {
     //Use the @PutMapping Annotation
     @PutMapping("/update/{id}")
     //Create  a method to update the employee records
-    public Optional<Employee> updateEmployeeRecords(@PathVariable Long id, @RequestBody Employee employee){
+    public ResponseEntity<String> updateEmployeeRecords(@PathVariable Long id, @RequestBody Employee employee){
+        //Use logger to give the warning
+        log.warn("Employee Id can't be changed");
         //return the value
         return useServices.updateRecord(id,employee);
     }
@@ -61,6 +67,9 @@ public class EmployeePayrollController {
     @DeleteMapping("/delete/{id}")
     //Create a method to delete the employee record by id
     public String deleteEmployeeRecord(@PathVariable Long id){
+
+        //Use logger to give the info.
+        log.info("Employee Record Deleting.");
         //call the method and return the result
         return useServices.deleteRecords(id)?"Record Deleted":"Record Not Found";
     }
